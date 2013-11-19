528badec8b56b001e814e5caf86fd60033d3a8a5
Diego Vidal <diefervidal91@gmail.com>
2013-11-19 13:08:53 -0500

    //Crear LoginViewController.h y LoadingViewController.h

---
+++ EventosCaracol/LoadingViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoadingViewController.m
+//  EventosCaracol
+//
+//  Created by Developer on 19/11/13.
+//  Copyright (c) 2013 iAmStudio. All rights reserved.
+//
+
+#import "LoadingViewController.h"
+
+@interface LoadingViewController ()
+
+@end
+
+@implementation LoadingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- EventosCaracol/LoginViewController.h
+++ EventosCaracol/LoginViewController.h
@@ -1,13 +1,13 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  EventosCaracol
 //
-//  Created by Andres Abril on 19/11/13.
+//  Created by Developer on 19/11/13.
 //  Copyright (c) 2013 iAmStudio. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface LoginViewController : UIViewController
 
 @end
