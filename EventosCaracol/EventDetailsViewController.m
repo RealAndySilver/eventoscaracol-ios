0389e9cb2ed2059ef9f52431bfa945fdffe211b3
Diego Vidal <diefervidal91@gmail.com>
2013-11-21 11:13:34 -0500

    //Sharing options (Facebook, Twitter, SMS, Mail) are now available in the Event detail view. (EventDetailsViewController) //The frame of the views in EventDetailsViewController has been modified for the correct visualization in both 3.5” & 4” devices.

--- EventosCaracol/EventDetailsViewController.m
+++ EventosCaracol/EventDetailsViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "EventDetailsViewController.h"
+#import <MessageUI/MessageUI.h>
+#import <Social/Social.h>
 
-@interface EventDetailsViewController ()
+@interface EventDetailsViewController () <UIActionSheetDelegate, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate>
 
 @end
 
@@ -20,51 +22,146 @@
     
     self.navigationItem.title = @"Detalles Evento";
     
+    //Create the UIBarButtonItem to share the event.
+    UIBarButtonItem *shareBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose
+                                                                                        target:self
+                                                                                        action:@selector(shareEvent)];
+    
+    self.navigationItem.rightBarButtonItem = shareBarButtonItem;
+    
     //Create the view's content and added it as subview of self.view
-    UIImageView *eventImage = [[UIImageView alloc] initWithFrame:CGRectMake(20.0, 77.0, 280.0, 200.0)];
+    
+    UIImageView *eventImage = [[UIImageView alloc]
+                               initWithFrame:CGRectMake(20.0,
+                                                        self.navigationController.navigationBar.frame.origin.y + self.navigationController.navigationBar.frame.size.height + 20.0,
+                                                        self.view.frame.size.width - 40.0,
+                                                        (self.view.frame.size.height/2 - 20) -(self.navigationController.navigationBar.frame.origin.y + self.navigationController.navigationBar.frame.size.height + 20.0))];
     eventImage.backgroundColor = [UIColor cyanColor];
     [self.view addSubview:eventImage];
     
-    UIButton *favoriteButton = [[UIButton alloc] initWithFrame:CGRectMake(20.0, 285.0, 44.0, 44.0)];
+    UIButton *favoriteButton = [[UIButton alloc] initWithFrame:CGRectMake(20.0,
+                                                                          self.view.frame.size.height/2,
+                                                                          40.0,
+                                                                          40.0)];
     [favoriteButton setTitle:@"Fav" forState:UIControlStateNormal];
     [favoriteButton setBackgroundColor:[UIColor purpleColor]];
     [self.view addSubview:favoriteButton];
     
-    UILabel *eventName = [[UILabel alloc] initWithFrame:CGRectMake(74.0, 285.0, 206, 44.0)];
+    UILabel *eventName = [[UILabel alloc] initWithFrame:CGRectMake(20.0 + favoriteButton.frame.size.width + 20,
+                                                                   self.view.frame.size.height/2,
+                                                                   self.view.frame.size.width - (20.0 + favoriteButton.frame.size.width + 20) - 20,
+                                                                   44.0)];
+    eventName.numberOfLines = 0;
     eventName.text = @"Desfile de las flores";
     eventName.font = [UIFont fontWithName:@"@Helvetica" size:15.0];
     [self.view addSubview:eventName];
     
-    UILabel *eventLocationLabel = [[UILabel alloc] initWithFrame:CGRectMake(74.0, 340.0, 200.0, 20.0)];
+    UILabel *eventLocationLabel = [[UILabel alloc] initWithFrame:CGRectMake(20.0 + favoriteButton.frame.size.width + 20,
+                                                                            eventName.frame.origin.y + eventName.frame.size.height,
+                                                                            self.view.frame.size.width - (20.0 + favoriteButton.frame.size.width + 20) - 20,
+                                                                            20.0)];
     eventLocationLabel.text = @"Plaza Cervantes";
     eventLocationLabel.font = [UIFont fontWithName:@"Helvetica" size:12.0];
     [self.view addSubview:eventLocationLabel];
     
-    UILabel *eventTimeLabel = [[UILabel alloc] initWithFrame:CGRectMake(74.0, 360.0, 200.0, 20.0)];
+    UILabel *eventTimeLabel = [[UILabel alloc] initWithFrame:CGRectMake(20.0 + favoriteButton.frame.size.width + 20,
+                                                                        eventLocationLabel.frame.origin.y + eventLocationLabel.frame.size.height,
+                                                                        self.view.frame.size.width - (20.0 + favoriteButton.frame.size.width + 20) - 20,
+                                                                        20.0)];
     eventTimeLabel.text = @"11:30AM";
     eventTimeLabel.font = [UIFont fontWithName:@"Helvetica" size:12.0];
     [self.view addSubview:eventTimeLabel];
     
+    UITextView *eventDescription = [[UITextView alloc]
+                                    initWithFrame:CGRectMake(20.0,
+                                                             eventTimeLabel.frame.origin.y + eventTimeLabel.frame.size.height + 10,
+                                                             self.view.frame.size.width - 40.0,
+                                                             self.view.frame.size.height - (eventTimeLabel.frame.origin.y + eventTimeLabel.frame.size.height + 10) - 20.0)];
     
-    UITextView *eventDescription = [[UITextView alloc] initWithFrame:CGRectMake(20.0,
-                                                                                self.view.frame.size.height/1.5,
-                                                                                self.view.frame.size.width-40,
-                                                                                self.view.frame.size.height-self.view.frame.size.height/1.5)];
     eventDescription.text = @"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non haben";
     eventDescription.font = [UIFont fontWithName:@"Helvetica" size:14.0];
     eventDescription.selectable = NO;
     eventDescription.editable = NO;
-    eventDescription.scrollEnabled = YES;
-    eventDescription.userInteractionEnabled = YES;
     [self.view addSubview:eventDescription];
 }
 
+-(void)shareEvent
+{
+    [[[UIActionSheet alloc] initWithTitle:nil
+                                delegate:self
+                       cancelButtonTitle:@"Volver"
+                  destructiveButtonTitle:nil
+                       otherButtonTitles:@"SMS", @"Facebook", @"Twitter", @"Correo", nil] showInView:self.view];
+}
 
+#pragma mark - UIActionSheetDelegate
 
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if(buttonIndex == 0)
+    {
+        NSLog(@"SMS");
+        if (![MFMessageComposeViewController canSendText])
+        {
+            [[[UIAlertView alloc] initWithTitle:@"Error"
+                                       message:@"No se pueden enviar mensajes desde este dispositivo"
+                                      delegate:self
+                             cancelButtonTitle:@"Ok"
+                             otherButtonTitles:nil] show];
+        }
+        
+        else
+        {
+            MFMessageComposeViewController *messageViewController = [[MFMessageComposeViewController alloc] init];
+            messageViewController.messageComposeDelegate = self;
+            [messageViewController setBody:@"Hola! te recomiendo este evento!"];
+            [self presentViewController:messageViewController animated:YES completion:nil];
+            NSLog(@"presenté el viewcontroller");
+        }
+    }
+    
+    else if (buttonIndex == 1)
+    {
+        NSLog(@"Facebook");
+        
+        SLComposeViewController *facebookViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        [facebookViewController setInitialText:@"Me acabo de inscribir al evento que vamos a ir todos."];
+        [self presentViewController:facebookViewController animated:YES completion:nil];
+    }
+    
+    else if (buttonIndex == 2)
+    {
+        NSLog(@"Twitter");
+        
+        SLComposeViewController *twitterViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [twitterViewController setInitialText:@"Me acbo de inscribir a este genial evento"];
+        [self presentViewController:twitterViewController animated:YES completion:nil];
+    }
+    
+    else if (buttonIndex == 3)
+    {
+        NSLog(@"Mail");
+        MFMailComposeViewController *mailComposeViewController = [[MFMailComposeViewController alloc] init];
+        [mailComposeViewController setSubject:@"Te recomiendo este evento!"];
+        [mailComposeViewController setMessageBody:@"¡Hola!, me acabo de inscribir en la presentación del evento al que todos vamos a ir. " isHTML:NO];
+        
+        mailComposeViewController.mailComposeDelegate = self;
+        [self presentViewController:mailComposeViewController animated:YES completion:nil];
+    }
+}
 
+#pragma mark - MFMailComposeViewControllerDelegate
 
+-(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
+#pragma mark - MFMessageComposeViewControllerDelegate
 
-
+-(void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
