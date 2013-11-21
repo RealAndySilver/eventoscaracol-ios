ba93b6d68e50b296cac6a48da73ca4957b5da0c8
Diego Vidal <diefervidal91@gmail.com>
2013-11-21 18:11:05 -0500

    //Create the buttons to filter the events by date and location, as well as the pickers that are display on screen when the user touches any of the buttons (EventsListViewController).

--- EventosCaracol/EventsListViewController.m
+++ EventosCaracol/EventsListViewController.m
@@ -13,25 +13,46 @@
 #import "SWTableViewCell.h"
 #import "Atom.h"
 
-@interface EventsListViewController () <UITableViewDataSource, UITableViewDelegate, SWTableViewCellDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate>{
+@interface EventsListViewController () <UITableViewDataSource, UITableViewDelegate, SWTableViewCellDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate, UIPickerViewDataSource, UIPickerViewDelegate>{
 }
 @property (strong, nonatomic)  UITableView *tableView;
 @property (strong, nonatomic) NSArray *eventsNamesTestArray; //Of NSString
-@property (strong, nonatomic) NSArray *eventsLocationTestArray; //Of NSString
-
+@property (strong, nonatomic) NSArray *eventsLocationsTestArray; //Of NSString
+@property (strong, nonatomic) NSArray *eventDatesArray; //Of NSString
+@property (strong, nonatomic) UIPickerView *locationPickerView;
+@property (strong, nonatomic) UIPickerView *datePickerView;
+@property (strong, nonatomic) UIView *containerLocationPickerView;
+@property (strong, nonatomic) UIView *containerDatesPickerView;
+@property (nonatomic) BOOL isPickerActivated;
 @end
 
 #define ROW_HEIGHT 90.0
 
 @implementation EventsListViewController
 
+//This Arrays are for test purposes only!!!
 //Lazy Instantiation
+-(NSArray *)eventDatesArray
+{
+    if (!_eventDatesArray)
+        _eventDatesArray = @[@"Todos los dias", @"Lunes", @"Martes", @"Miercoles", @"Jueves", @"Viernes", @"Sabado", @"Domingo"];
+    
+    return _eventDatesArray;
+}
+
 -(NSArray *)eventsNamesTestArray
 {
     if (!_eventsNamesTestArray)
         _eventsNamesTestArray = @[@"Desfile de las flores", @"Desfile de tulipanes", @"desfile de rosas", @"desfile de petunias", @"Fiesta al  parque"];
     
     return _eventsNamesTestArray;
+}
+
+-(NSArray *)eventsLocationsTestArray
+{
+    if (!_eventsLocationsTestArray)
+        _eventsLocationsTestArray = @[@"Todos los lugares", @"Lugar1", @"Lugar2", @"Lugar3", @"Lugar4", @"Lugar5"];
+    return _eventsLocationsTestArray;
 }
 
 #pragma mark - View LifeCycle
@@ -45,19 +66,121 @@
                                                                              style:UIBarButtonItemStylePlain
                                                                             target:self
                                                                             action:nil];
+    self.navigationItem.title = @"Programación";
     
-    self.tableView=[[UITableView alloc]initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.origin.y+self.navigationController.navigationBar.frame.size.height, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    ///////////////////////////////////////////////////////////////////
+    //Create two buttons to filter the events list by date and by location
+    //Filter by date button
+    UIButton *filterByDayButton = [[UIButton alloc]
+                                   initWithFrame:CGRectMake(0,
+                                                            self.navigationController.navigationBar.frame.origin.y + self.navigationController.navigationBar.frame.size.height,
+                                                            self.view.frame.size.width/2,
+                                                            44.0)];
     
+    //We need to set the button tag of filterByDayButton and filterByLocationButton to show the correct picker
+    //when the user touches one of these buttons.
+    filterByDayButton.tag = 1;
+    
+    [filterByDayButton addTarget:self action:@selector(showPickerView:) forControlEvents:UIControlEventTouchUpInside];
+    [filterByDayButton setTitle:@"Todos los dias" forState:UIControlStateNormal];
+    filterByDayButton.titleLabel.font = [UIFont fontWithName:@"Helvetica" size:14.0];
+    filterByDayButton.backgroundColor = [UIColor cyanColor];
+    [self.view addSubview:filterByDayButton];
+    
+    //Filter by location button
+    UIButton *filterByLocationButton = [[UIButton alloc]
+                                        initWithFrame:CGRectMake(self.view.frame.size.width/2,
+                                                                 self.navigationController.navigationBar.frame.origin.y + self.navigationController.navigationBar.frame.size.height,
+                                                                 self.view.frame.size.width/2,
+                                                                 44.0)];
+    filterByLocationButton.tag = 2;
+    
+    [filterByLocationButton addTarget:self action:@selector(showPickerView:) forControlEvents:UIControlEventTouchUpInside];
+    filterByLocationButton.backgroundColor = [UIColor cyanColor];
+    filterByLocationButton.titleLabel.font = [UIFont fontWithName:@"Helvetica" size:14.0];
+    [filterByLocationButton setTitle:@"Todos los lugares" forState:UIControlStateNormal];
+    [self.view addSubview:filterByLocationButton];
+    
+    ///////////////////////////////////////////////////////////////////
+    //Table View initialization and configuration
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, filterByLocationButton.frame.origin.y + filterByLocationButton.frame.size.height, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
     [self.view addSubview:_tableView];
     
-    //Table View Config
+    
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     self.tableView.rowHeight = ROW_HEIGHT;
     self.tableView.allowsSelection = YES;
     self.tableView.separatorInset = UIEdgeInsetsMake(0.0, 0.0, 0.0, 0.0);
     
-    self.navigationItem.title = @"Programación";
+    ///////////////////////////////////////////////////////////////////
+    //Configure locationPickerView
+    self.locationPickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0.0,
+                                                                             0.0,
+                                                                             self.containerLocationPickerView.frame.size.width,
+                                                                             self.containerLocationPickerView.frame.size.height)];
+    self.locationPickerView.tag = 2;
+    self.locationPickerView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:1.0];
+    self.locationPickerView.delegate = self;
+    self.locationPickerView.dataSource = self;
+    
+    /////////////////////////////////////////////////////////////////
+    //Configure datePickerView
+    self.datePickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0.0,
+                                                                         0.0,
+                                                                         self.containerLocationPickerView.frame.size.width,
+                                                                         self.containerLocationPickerView.frame.size.height)];
+    self.datePickerView.tag = 1;
+    self.datePickerView.delegate = self;
+    self.datePickerView.dataSource = self;
+    
+    ////////////////////////////////////////////////////////////////////
+    //Configure container view for the Location Picker
+    self.containerLocationPickerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, self.view.frame.size.height, self.view.frame.size.width, self.view.frame.size.height/2.5)];
+    self.containerLocationPickerView.backgroundColor = [UIColor whiteColor];
+    [self.containerLocationPickerView addSubview:self.locationPickerView];
+    
+    //////////////////////////////////////////////////////////////////
+    //Configure container view for the Dates picker.
+    self.containerDatesPickerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, self.view.frame.size.height, self.view.frame.size.width, self.view.frame.size.height/2.5)];
+    self.containerDatesPickerView.backgroundColor = [UIColor whiteColor];
+    [self.containerDatesPickerView addSubview:self.datePickerView];
+
+    
+    /////////////////////////////////////////////////////////////////
+    //Create a button to dismiss the location picker view.
+    UIButton *dismissLocationPickerButton = [[UIButton alloc] initWithFrame:CGRectMake(self.containerLocationPickerView.frame.size.width - 40.0, self.containerLocationPickerView.frame.size.height - 40.0, 40.0, 40.0)];
+    dismissLocationPickerButton.tag = 2;
+    dismissLocationPickerButton.backgroundColor = [UIColor purpleColor];
+    [dismissLocationPickerButton addTarget:self action:@selector(showPickerView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.containerLocationPickerView addSubview:dismissLocationPickerButton];
+    
+    /////////////////////////////////////////////////////////////////
+    UIButton *dismissDatePickerButton = [[UIButton alloc] initWithFrame:CGRectMake(self.containerDatesPickerView.frame.size.width - 40.0,self.containerDatesPickerView.frame.size.height - 40.0, 40.0, 40.0)];
+    dismissDatePickerButton.tag = 1;
+    dismissDatePickerButton.backgroundColor = [UIColor purpleColor];
+    [dismissDatePickerButton addTarget:self action:@selector(showPickerView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.containerDatesPickerView addSubview:dismissDatePickerButton];
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    //If any of the picker containers views is on view, remove it.
+    if ([self.containerLocationPickerView isDescendantOfView:self.view])
+    {
+        NSLog(@"ContainerLocationPickerView estaba en self.view");
+        [self.containerLocationPickerView removeFromSuperview];
+    }
+    
+    if ([self.containerDatesPickerView isDescendantOfView:self.view])
+    {
+        NSLog(@"ContainerDatesPickerView estaba en self.view ");
+        [self.containerDatesPickerView removeFromSuperview];
+    }
+    
+    //We have to set isPickerActivated to NO, so when the user come back to this view and press any of the buttons to
+    //show the picker, it will animate.
+    self.isPickerActivated = NO;
 }
 
 #pragma mark - UITableViewDelegate & UITableViewDataSource
@@ -179,7 +302,6 @@
         SLComposeViewController *twitterViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
         [twitterViewController setInitialText:@"Me acbo de inscribir a este genial evento"];
         [self presentViewController:twitterViewController animated:YES completion:nil];
- 
     }
     
     else if (buttonIndex == 3)
@@ -206,6 +328,98 @@
 -(void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - UIPickerViewDelegate & UIPickerViewDataSource
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if (pickerView.tag == 1)
+        return [self.eventDatesArray count];
+    
+    else if (pickerView.tag == 2)
+        return 5;
+    
+    else return 0;
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if (pickerView.tag == 1)
+        return self.eventDatesArray[row];
+    
+    else if (pickerView.tag == 2)
+        return self.eventsLocationsTestArray[row];
+    else
+        return nil;
+}
+
+#pragma mark - Actions
+
+-(void)showPickerView:(id)sender
+{
+    UIView *containerView = nil;
+    if ([sender tag] == 1)
+        containerView = self.containerDatesPickerView;
+    
+    else if ([sender tag] == 2)
+        containerView = self.containerLocationPickerView;
+        
+    //If pickerIsActivated = NO, create and animation to show the picker on screen.
+    if (!self.isPickerActivated)
+    {
+        [self.view addSubview:containerView];
+        NSLog(@"me oprimi");
+        [UIView animateWithDuration:0.3
+                              delay:0.0
+                            options: UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             
+                             //Bring up the corresponding container view.
+                             
+                             containerView.transform = CGAffineTransformMakeTranslation(0.0, -containerView.frame.size.height);
+                             
+                         }
+                         completion:^(BOOL finished){
+                             
+                         }
+         
+         ];
+        
+        self.isPickerActivated = YES;
+    }
+    
+    //else, create and animation to hide it from screen.
+    else
+    {
+        [UIView animateWithDuration:0.3
+                              delay:0.0
+                            options: UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             
+                             //bring searchView up
+                             containerView.transform = CGAffineTransformMakeTranslation(0.0, containerView.frame.size.height);
+                             
+                         }
+                         completion:^(BOOL finished){
+                             [containerView removeFromSuperview];
+                         }
+         
+         ];
+        
+        self.isPickerActivated = NO;
+
+    }
 }
 
 @end
