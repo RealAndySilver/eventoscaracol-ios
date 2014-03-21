2005a2c97eb02e788b2f6f41c60ff5e9b958541d
Andres Abril <andres.abril@gmail.com>
2014-03-20 20:20:40 -0500

    - Se agregó la clase iAmCoder, la cual se encarga de hacer el sha256 y el base64 para la cabecera de la petición - Se agregó en ServerCommunicator una función que permite agregar items necesarios para la autenticación en la cabecera - Se modificó la manera de realizar la petición en el modo get del ServerCommunicator

--- EventosCaracol/ServerCommunicator.m
+++ EventosCaracol/ServerCommunicator.m
@@ -7,7 +7,9 @@
 //
 
 #import "ServerCommunicator.h"
-#define ENDPOINT @"http://iccknet-eventoscaracol.jit.su"
+#import "IAmCoder.h"
+#define ENDPOINT @"http://iccknet-eventoscaracol.jit.su/api_1.0"
+//#define ENDPOINT @"http://10.0.1.6:8080/api_1.0"
 //#define ENDPOINT @"http://caracol.aws.af.cm/"
 //#define ENDPOINT @"http://10.0.1.9:8080"
 //#define ENDPOINT @"http://iamstudio-sweetwater.herokuapp.com/"
@@ -27,34 +29,32 @@
     parameter=[parameter stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     parameter=[parameter stringByExpandingTildeInPath];
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/%@/%@",ENDPOINT,method,parameter]];
-	NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
-    [theRequest setValue:@"application/json" forHTTPHeaderField:@"accept"];
+	NSMutableURLRequest *theRequest = [self getHeaderForUrl:url];
     
     NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration:defaultConfigObject
                                                                  delegate:nil
                                                             delegateQueue:[NSOperationQueue mainQueue]];
     
-    NSURLSessionDataTask * dataTask = [defaultSession dataTaskWithURL:url
-                                                    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
-                                                        if(error == nil){
-                                                            NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                                                            [self.delegate receivedDataFromServer:dictionary
-                                                                                   withMethodName:method];
-                                                        }
-                                                        else{
-                                                            [self.delegate serverError:error];
-                                                        }
-                                                    }];
+    NSURLSessionDataTask * dataTask = [defaultSession dataTaskWithRequest:theRequest
+                                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
+                                                            if(error == nil){
+                                                                NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                                                                [self.delegate receivedDataFromServer:dictionary
+                                                                                       withMethodName:method];
+                                                            }
+                                                            else{
+                                                                [self.delegate serverError:error];
+                                                            }
+                                                        }];
     [dataTask resume];
 }
 -(void)callServerWithPOSTMethod:(NSString *)method andParameter:(NSString *)parameter httpMethod:(NSString *)httpMethod{
     parameter=[parameter stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     parameter=[parameter stringByExpandingTildeInPath];
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/%@",ENDPOINT,method]];
-	NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+	NSMutableURLRequest *theRequest = [self getHeaderForUrl:url];
     [theRequest setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-    [theRequest setValue:@"application/json" forHTTPHeaderField:@"accept"];
     [theRequest setHTTPMethod:httpMethod];
     NSData *data=[NSData dataWithBytes:[parameter UTF8String] length:[parameter length]];
     [theRequest setHTTPBody: data];
@@ -75,5 +75,18 @@
                                                         }
                                                     }];
     [dataTask resume];
+}
+#pragma mark - http header
+-(NSMutableURLRequest*)getHeaderForUrl:(NSURL*)url{
+    NSString *key=@"lop+2dzuioa/000mojijiaop";
+    NSString *time=[IAmCoder dateString];
+    NSString *encoded=[NSString stringWithFormat:@"%@",[IAmCoder sha256:[NSString stringWithFormat:@"%@%@",key,time]]];
+	NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    [theRequest setValue:@"application/json" forHTTPHeaderField:@"accept"];
+    [theRequest setValue:[NSString stringWithFormat:@"%@",[IAmCoder base64String:key]] forHTTPHeaderField:@"C99-RSA"];
+    [theRequest setValue:[NSString stringWithFormat:@"%@",[IAmCoder base64String:time]] forHTTPHeaderField:@"SSL"];
+    [theRequest setValue:encoded forHTTPHeaderField:@"token"];
+    NSLog(@"Header %@\nTime %@",theRequest.allHTTPHeaderFields,time);
+    return theRequest;
 }
 @end
